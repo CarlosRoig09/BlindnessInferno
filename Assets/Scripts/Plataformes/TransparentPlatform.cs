ffe297bf125a7312fe4bb93d8e14a00b145dc4d8
lidia.puigmitja.7e5 <lidia.puigmitja.7e5@itb.cat>
2022-12-16 16:48:32 +0100

    screenshake y transparencia platforms done.

---
+++ Assets/Scripts/Plataformes/TransparentPlatform.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentPlatform : PlatformControler
+{
+
+
+    protected override void PlatformHability()
+    {
+        for (float alpha = 1; alpha > -0.1; alpha -= 0.1f)
+        {
+
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, alpha);
+
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlatformHability();
+        }
+    }
+
+}
