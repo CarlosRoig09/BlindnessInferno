40245a613b539fa5ee49c9dd0ed655eafb4c40b6
lidia.puigmitja.7e5 <lidia.puigmitja.7e5@itb.cat>
2022-12-19 16:00:13 +0100

    prefabs de plataformas

--- Assets/Scripts/Plataformes/PlatformManager.cs
+++ Assets/Scripts/Plataformes/PlatformManager.cs
@@ -6,15 +6,18 @@ public class PlatformManager : MonoBehaviour
 {
     private Vector3 _initPosition;
     [SerializeField]
-    private PlatformScriptableObject _plSO;
+    private PlatformScriptableObject[] _plSO;
     private float _counter;
     private ObstacleManager _obstacleManager;
     private EnemySpawn _enemySpawn;
+
+    private int num;
     // Start is called before the first frame update
     void Start()
     {
-       _initPosition = new Vector3((Camera.main.transform.position.x + (2f*Camera.main.orthographicSize * Camera.main.aspect))/2 + _plSO.PlatformPrefab.transform.localScale.x, _plSO.PlatformInitialYPosition);
-        _counter = _plSO.PlatgormWaitTimer;
+        num = Random.Range(0, 3);
+        _initPosition = new Vector3((Camera.main.transform.position.x + (2f*Camera.main.orthographicSize * Camera.main.aspect))/2 + _plSO[num].PlatformPrefab.transform.localScale.x, _plSO[num].PlatformInitialYPosition);
+        _counter = _plSO[num].PlatgormWaitTimer;
         _obstacleManager = gameObject.GetComponent<ObstacleManager>();
         _enemySpawn = gameObject.GetComponent<EnemySpawn>();
     }
@@ -22,15 +25,21 @@ public class PlatformManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_plSO.PlatgormWaitTimer <= _counter)
+       num = Random.Range(0, 3);
+ 
+        if (_plSO[num].PlatgormWaitTimer <= _counter)
         {
-           var platform = Instantiate(_plSO.PlatformPrefab, _initPosition, Quaternion.identity);
+           var platform = Instantiate(_plSO[num].PlatformPrefab, _initPosition, Quaternion.identity);
            // _obstacleManager.Instantiate(platform);
             _enemySpawn.Instantiate(platform);
             _counter = 0;
        }
         else
+        {
             _counter += Time.deltaTime;
+        }
+
+      
 
     }
 }
