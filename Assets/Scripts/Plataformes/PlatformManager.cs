5e76872ff1119df1fe2082ebe806bdac937bd991
lidia puigmitja ruiz [itb] <lidia.puigmitja.7e5@itb.cat>
2022-12-21 15:29:55 +0000

    Replace PlatformManager.cs

--- Assets/Scripts/Plataformes/PlatformManager.cs
+++ Assets/Scripts/Plataformes/PlatformManager.cs
@@ -2,44 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformManager : MonoBehaviour
+public class GeneratePlatformController : MonoBehaviour
 {
     private Vector3 _initPosition;
     [SerializeField]
-    private PlatformScriptableObject[] _plSO;
+    private PlatformScriptableObject _plSO;
     private float _counter;
     private ObstacleManager _obstacleManager;
     private EnemySpawn _enemySpawn;
-
-    private int num;
     // Start is called before the first frame update
     void Start()
     {
-        num = Random.Range(0, 3);
-        _initPosition = new Vector3((Camera.main.transform.position.x + (2f*Camera.main.orthographicSize * Camera.main.aspect))/2 + _plSO[num].PlatformPrefab.transform.localScale.x, _plSO[num].PlatformInitialYPosition);
-        _counter = _plSO[num].PlatgormWaitTimer;
+        _counter = _plSO.PlatgormWaitTimer;
         _obstacleManager = gameObject.GetComponent<ObstacleManager>();
         _enemySpawn = gameObject.GetComponent<EnemySpawn>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-       num = Random.Range(0, 4);
- 
-        if (_plSO[num].PlatgormWaitTimer <= _counter)
+        if (_plSO.PlatgormWaitTimer/Camera.main.GetComponent<MoveCamara>()._rb.velocity.x <= _counter)
         {
-           var platform = Instantiate(_plSO[num].PlatformPrefab, _initPosition, Quaternion.identity);
-           // _obstacleManager.Instantiate(platform);
-            _enemySpawn.Instantiate(platform);
+            _initPosition = new Vector3(transform.position.x, _plSO.PlatformInitialYPosition);
+            var platform = Instantiate(_plSO.PlatformPrefab, _initPosition, Quaternion.identity);
+            // _obstacleManager.Instantiate(platform);
+            // _enemySpawn.Instantiate(platform);
             _counter = 0;
-       }
-        else
-        {
-            _counter += Time.deltaTime;
         }
-
-      
-
+        else
+            _counter += Time.deltaTime;
     }
 }
