f7385374e7f12131c5cb7abbac87b83e0e09e4d6
Carlos Roig Garcia <carlos.roig.7e5@itb.cat>
2023-01-10 19:50:01 +0100

    ItemRandomized

---
+++ Assets/ScriptableObject/FlyEnemySO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FlyEnemySO", menuName = "FlyEnemySO")]
+public class FlyEnemySO : RandomSO
+{
+    public float FlyingSpeed;
+    public float AttackSpeed;
+    public float TimeChaangeDirection;
+}
---
+++ Assets/Scripts/Randomitzator/RandomSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class RandomSO : ScriptableObject
+{
+    public float RateAperance;
+}
