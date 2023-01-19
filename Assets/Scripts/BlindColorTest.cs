298353aea214d535a3fe6feb23f8e20585a6322d
lidia.puigmitja.7e5 <lidia.puigmitja.7e5@itb.cat>
2023-01-19 22:08:01 +0100

    queda pulir rayo i plataforma nivell 3 DONE!

--- Assets/Scripts/BlindColorTest.cs
+++ Assets/Scripts/BlindColorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,11 +28,14 @@ public class BlindColorTest : MonoBehaviour
     private float _distance;
     private float _distanciaMax;
     private float _count;
+    private float _numRayo;
     public float DistanciaMax
     {
-        get=>_distanciaMax;
-        set=>_distanciaMax = value;
+        get => _distanciaMax;
+        set => _distanciaMax = value;
     }
+
+    public GameObject rayo;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,13 +43,16 @@ public class BlindColorTest : MonoBehaviour
         _distance = GameObject.Find("Kilomiters").GetComponent<Kilomiters>().Distancia;
         _colorBlind.Type = 1;
         _count = 0;
+        _numRayo = 0;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         _distance = GameObject.Find("Kilomiters").GetComponent<Kilomiters>().Distancia;
-      
+
 
         if (_colorBlind.Type == 0 && _distance <= _distanciaMax)
         {
@@ -55,6 +62,7 @@ public class BlindColorTest : MonoBehaviour
         {
             if (_count >= 0f && _count < 500f)
             {
+                _numRayo = 0;
                 Nivel = Niveles.Nivel1;
                 _colorBlind.Type = 1;
             }
@@ -64,12 +72,15 @@ public class BlindColorTest : MonoBehaviour
                 if (_count >= 550f)
                 {
                     _colorBlind.Type = 2;
+                    if (_numRayo == 0)
+                        StartCoroutine(ChargeRayo(3f));
 
                 }
-               
+
             }
             else if (_count >= 600f && _count < 1100f)
             {
+                _numRayo = 0;
                 Nivel = Niveles.Nivel2;
                 _colorBlind.Type = 2;
 
@@ -80,12 +91,15 @@ public class BlindColorTest : MonoBehaviour
                 if (_count >= 1150f)
                 {
                     _colorBlind.Type = 3;
+                    if (_numRayo == 0)
+                        StartCoroutine(ChargeRayo(3f));
 
                 }
 
             }
             else if (_count >= 1200f && _count < 1700f)
             {
+                _numRayo = 0;
                 Nivel = Niveles.Nivel3;
                 _colorBlind.Type = 3;
             }
@@ -95,12 +109,15 @@ public class BlindColorTest : MonoBehaviour
                 if (_count >= 1750f)
                 {
                     _colorBlind.Type = 3;
+                    if (_numRayo == 0)
+                        StartCoroutine(ChargeRayo(3f));
 
                 }
 
             }
             else if (_count >= 1700f && _count < 2200f)
             {
+                _numRayo = 0;
                 Nivel = Niveles.Nivel3;
                 _colorBlind.Type = 3;
             }
@@ -110,16 +127,23 @@ public class BlindColorTest : MonoBehaviour
                 if (_count >= 2250f)
                 {
                     _colorBlind.Type = 1;
+                    if (_numRayo == 0)
+                        StartCoroutine(ChargeRayo(3f));
 
                 }
 
             }
             else _count = 0;
         }
-        _count += 10*Time.deltaTime;
+        _count += 10 * Time.deltaTime;
         //Debug.Log(_count);
     }
 
- 
-
+    public IEnumerator ChargeRayo(float time)
+    {
+        Instantiate(rayo, gameObject.transform.position, Quaternion.identity);
+        _numRayo = 1;
+        yield return new WaitForSeconds(time);
+        Destroy(rayo);
+    }
 }
--- Assets/Scripts/Plataformes/GeneratePlatformController.cs
+++ Assets/Scripts/Plataformes/GeneratePlatformController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 
@@ -30,7 +32,8 @@ public class GeneratePlatformController : MonoBehaviour
     private AlPamPamYAlPiumPium _enemySpawn;
 
     private Niveles _nivel;
- 
+
+    private PlatformScriptableObject[] _plSONivel2y3;
 
     // Start is called before the first frame update
     void Start()
@@ -39,9 +42,15 @@ public class GeneratePlatformController : MonoBehaviour
         _plSO = _plSONivel1;
         _num = RandomMethods.ReturnARandomObject(_plSO, 0);
         if (_num < 0)
-            _num = Random.Range(0, _plSO.Length);
+            _num = UnityEngine.Random.Range(0, _plSO.Length);
         _counter = _plSO[_num].PlatgormWaitTimer;
         _enemySpawn = gameObject.GetComponent<AlPamPamYAlPiumPium>();
+        _plSONivel2y3 = GenerateNevel3();
+        for(int i=0; i < _plSONivel2y3.Length;i++)
+        {
+            Debug.Log(_plSONivel2y3[i]);
+        }
+        
 
     }
 
@@ -57,6 +66,24 @@ public class GeneratePlatformController : MonoBehaviour
         else
             _counter += Time.deltaTime;
     }
+    private PlatformScriptableObject[] GenerateNevel3()
+    {
+        var position = (_plSONivel2.Length); 
+        var position2= position + (_plSONivel3.Length);
+        Array.Resize(ref _plSONivel2y3, position2);
+       
+        for (int i = 0; i < position; i++)
+        {
+            _plSONivel2y3[i] = _plSONivel2[i];
+
+        }
+        for(int i=_plSONivel2.Length;i< position2; i++)
+        {
+            _plSONivel2y3[i] = _plSONivel3[i-_plSONivel2.Length];
+        }
+        return _plSONivel2y3;
+
+    }
 
     private void GeneratePlatform()
     {
@@ -64,7 +91,7 @@ public class GeneratePlatformController : MonoBehaviour
         switch (_nivel)
         {
             case Niveles.Nivel1:
-                _plSO = _plSONivel1;
+                _plSO = _plSONivel2y3;
                 break;
             case Niveles.TransNivel1:
                 _plSO = _plSOTransition;
@@ -76,13 +103,13 @@ public class GeneratePlatformController : MonoBehaviour
                 _plSO = _plSOTransition;
                 break;
             case Niveles.Nivel3:
-                _plSO = _plSONivel3;
+                _plSO = _plSONivel1;
                 break;
             case Niveles.TransNivel3:
                 _plSO = _plSOTransition;
                 break;
             case Niveles.NivelBoss:
-                _plSO = _plSONivel3;
+                _plSO = _plSONivel1;
                 break;
             case Niveles.TransNivelBoss:
                 _plSO = _plSOLastTransition;
@@ -94,7 +121,7 @@ public class GeneratePlatformController : MonoBehaviour
 
         _num = RandomMethods.ReturnARandomObject(_plSO, 0);
         if (_num < 0)
-            _num = Random.Range(0, _plSO.Length);
+            _num = UnityEngine.Random.Range(0, _plSO.Length);
         _initPosition = new Vector3(transform.position.x, _plSO[_num].PlatformInitialYPosition);
         var platform = Instantiate(_plSO[_num].PlatformPrefab, _initPosition, Quaternion.identity);
         _counter = 0;
