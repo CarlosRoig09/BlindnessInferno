e03651204955280c0c3e8e58e853d0c39efc7797
lidia.puigmitja.7e5 <lidia.puigmitja.7e5@itb.cat>
2023-01-22 16:42:45 +0100

    collider arreglado, rayos añadidos

--- Assets/Scripts/DestroyObjects.cs
+++ Assets/Scripts/DestroyObjects.cs
@@ -4,14 +4,17 @@ public class DestroyObjects : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
+      
+        if(collision.gameObject.layer==6||collision.CompareTag("Enemy")||collision.CompareTag("Bullet"))
+           Destroy(collision.gameObject);
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<ControlDeath>().Life = Life.Death;
-        }
-        else
-        {
-            if(collision.gameObject.layer==6||collision.CompareTag("Enemy")||collision.CompareTag("Bullet"))
-             Destroy(collision.gameObject);
+            Debug.Log("Morí");
         }
     }
 }
